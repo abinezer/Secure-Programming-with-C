947fe2d73e00a557288611809890b5632bf34dc7
Abishai Ebenezer <44427255+abinezer@users.noreply.github.com>
2020-04-24 19:20:52 +0530

    Add files via upload

---
+++ MAIN.C
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<C:\Abishai\SecureC_project\PbAbi.h>
+char n;
+int* ptr;
+int main()
+{
+	/*
+	int flag = 0;
+	int cnt = 0;
+	int false1 = 0;
+	int fptr;
+	int* ptr;
+	int rno;
+	int year;
+	int maxrollno;
+	int num1;
+	char branch[6], br1[6];
+	char n;
+	*/
+	//clrscr
+
+	while(1)
+	{
+		//clrscr();
+		printf("\n\n\t\t\tWelcome to Voting System\n");
+
+		printf("\n\t\t\t   1.Student panel \n\t\t\t   2.Admin panel \n\t\t\t   3.Exit \n\t\t\t   Option:");
+		scanf(" %c",&n);
+
+
+		if(n==49)
+				stu();
+		else
+		{	if(n==50)
+				admin();
+			else
+			{
+				if(n==51)
+				{
+					free(ptr);
+					free(a);
+					return 0;
+				}
+				else
+				{
+					printf("\nInvalid option");
+					continue;
+				}
+			}
+		}
+	}
+	return 0;
+}
